2a3dbe3574f1153b527b596773831e4e77df987f
umarfarid8 <umerfarid034@gmail.com>
2025-02-08 11:55:59 +0500

    07-02-2025

---
+++ app/src/main/java/com/UZdevelopers/sarghodhapc/FragmentWeightmentAssistant.kt
@@ -0,0 +1,20 @@
+package com.UZdevelopers.sarghodhapc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FragmentWeightmentAssistant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fragment_weightment_assistant)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
