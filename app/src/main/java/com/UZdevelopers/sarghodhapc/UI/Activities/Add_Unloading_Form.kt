3bf29ebe59cd570b90b469b8618df9bc1384296f
zhairAhmad <ahmadzhair2003@gmail.com>
2025-02-09 22:12:50 +0500

    some activities

---
+++ app/src/main/java/com/UZdevelopers/sarghodhapc/UI/Activities/Add_Unloading_Form.kt
@@ -0,0 +1,21 @@
+package com.UZdevelopers.sarghodhapc.UI.Activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.UZdevelopers.sarghodhapc.R
+
+class Add_Unloading_Form : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_unloading_form)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/UZdevelopers/sarghodhapc/UI/Activities/SelectMaterial.kt
+++ app/src/main/java/com/UZdevelopers/sarghodhapc/UI/Activities/SelectMaterial.kt
@@ -1,21 +1,48 @@
 package com.UZdevelopers.sarghodhapc.UI.Activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.UZdevelopers.sarghodhapc.R
+import com.UZdevelopers.sarghodhapc.databinding.ActivitySelectMaterialBinding
 
 class SelectMaterial : AppCompatActivity() {
+    lateinit var  binding: ActivitySelectMaterialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_select_material)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+           binding= ActivitySelectMaterialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardView1.setOnClickListener{
+            val intent= Intent(this,Add_Weighment_Form::class.java)
+            intent.putExtra("material","Brassica Straw Loose")
+            startActivity(intent)
         }
+
+        binding.cardView.setOnClickListener{
+            val intent= Intent(this,Add_Weighment_Form::class.java)
+            intent.putExtra("material","Wheat Straw Loose CTMP")
+            startActivity(intent)
+        }
+        binding.cardView3.setOnClickListener{
+            val intent= Intent(this,Add_Weighment_Form::class.java)
+            intent.putExtra("material","Wheat Straw Loose Biomass")
+            startActivity(intent)
+        }
+        binding.cardView4.setOnClickListener{
+            val intent= Intent(this,Add_Weighment_Form::class.java)
+            intent.putExtra("material","Linseed Straw Loose")
+            startActivity(intent)
+        }
+        binding.cardView5.setOnClickListener{
+            val intent= Intent(this,Add_Quality_form::class.java)
+            intent.putExtra("material","Corn")
+            startActivity(intent)
+        }
+
+
     }
 }
